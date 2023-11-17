fd53417fef88e58e7ca1fe2c7b565fbebba33fff
Mark Keeble <mjkeeble@yahoo.de>
2023-11-17 18:47:40 +0100

    revise navbar, make logo clickable

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -14,14 +14,6 @@ export const Navbar: React.FC = () => {
     <>
       <div className="lg:hidden block absolute top-14 w-full left-0 right-0 bg-white transition ">
         <ul className="text-center pt-4 text-[24px]">
-          <Link spy={true} smooth={true} to="Home">
-            <li
-              className="py-4 text-blue-600 border-b border-charcoal-800 hover:bg-charcoal-800 hover:rounded cursor-pointer"
-              onClick={handleClick}
-            >
-              Home
-            </li>
-          </Link>
           <Link spy={true} smooth={true} to="About">
             <li
               className="py-4 text-blue-600 border-b border-charcoal-800 hover:bg-charcoal-800 hover:rounded cursor-pointer"
@@ -79,16 +71,13 @@ export const Navbar: React.FC = () => {
     <nav className="sticky top-0 bg-white z-[100]">
       <div className="h-10vh flex justify-between z-50 text-blue-600 px-5 py-4">
         <div className="flex items-center flex-1">
+        <Link spy={true} smooth={true} to="Home">
           <img className="h-9" src="/mjk_logo_cv.svg" alt="" />
+          </Link>
         </div>
         <div className="lg:flex lg:flex-1 items-center justify-end font-normal hidden">
           <div className="flex-10">
             <ul className="flex gap-8 mr-16 text-[24px]">
-              <Link spy={true} smooth={true} to="Home">
-                <li className="text-blue-600 hover:text-blue-300 border-b-2 border-white hover:border-blue-300 transition cursor-pointer">
-                  Home
-                </li>
-              </Link>
               <Link spy={true} smooth={true} to="About">
                 <li className="text-blue-600 hover:text-blue-300 border-b-2 border-white hover:border-blue-300 transition cursor-pointer">
                   About
