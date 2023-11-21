423c5b2d3e4295c674fd54f86b6455b52b67a08e
Mark Keeble <mjkeeble@yahoo.de>
2023-11-21 11:58:13 +0100

    hero text animation

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,15 +1,25 @@
+import { Fade } from "react-awesome-reveal";
+
 export const Hero: React.FC = () => {
   return (
     <header className="w-full  h-[100svh] h-[100vh] text-white grid grid-cols-1 grid-rows-2 lg:grid-cols-2 lg:grid-rows-1">
       <div className="h-[calc(100svh/2)] lg:h-screen flex flex-col justify-center lg:pr-8 lg:py-9 font-mjk">
-        <h1 className="text-blue-600 text-center lg:text-right text-4xl min-[445px]:text-6xl sm:text-7xl md:text-8xl lg:text-8xl xl:text-9xl font-bold">
-          Web Development
-          <br />
-          in
-        </h1>
-        <h1 className="text-peach-200 text-center lg:text-right text-[80px] min-[445px]:text-[120px] sm:text-[150px] lg:text-[140px] xl:text-[180px] font-black">
+        <Fade cascade duration={1000}>
+          <h1 className="text-blue-600 text-center lg:text-right text-4xl min-[445px]:text-6xl sm:text-7xl md:text-8xl lg:text-8xl xl:text-9xl font-bold">
+            Web Development
+          </h1>
+          <h1 className="text-blue-600 text-center lg:text-right text-4xl min-[445px]:text-6xl sm:text-7xl md:text-8xl lg:text-8xl xl:text-9xl font-bold">
+            in
+          </h1>
+        </Fade>
+        <Fade
+          className="text-peach-200 text-center lg:text-right text-[80px] min-[445px]:text-[120px] sm:text-[150px] lg:text-[140px] xl:text-[180px] font-black"
+          delay={1e3}
+          cascade
+          damping={0.2}
+        >
           Context
-        </h1>
+        </Fade>
       </div>
       <div className="relative bg-hero bg-center bg-cover h-[calc(100svh/2)] lg:h-screen"></div>
       <p className="absolute bottom-4 lg:bottom-4 right-5 lg:right-6 text-white text-sm md:text-md">
