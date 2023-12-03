ba821d125ab66aa0c58136e1346390a37a84aae4
Mark J. Keeble <66460031+mjkeeble@users.noreply.github.com>
2023-12-03 11:47:48 +0100

    Update slick.tsx
    Change scroll settings

--- src/components/about/slick.tsx
+++ src/components/about/slick.tsx
@@ -65,29 +65,32 @@ export default function SimpleSlider() {
     arrows: false,
     className: "px-10",
     infinite: true,
-    speed: 2500,
+    speed: 1000,
     slidesToShow: 5,
-    slidesToScroll: 1,
+    slidesToScroll: 5,
     autoplay: true,
-    autoplaySpeed: 10,
+    autoplaySpeed: 3000,
     pauseOnHover: false,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 4,
+          slidesToScroll: 4,
         },
       },
       {
         breakpoint: 768,
         settings: {
           slidesToShow: 3,
+          slidesToScroll: 3,
         },
       },
       {
         breakpoint: 540,
         settings: {
           slidesToShow: 1,
+          slidesToScroll: 1,
         },
       },
     ],
